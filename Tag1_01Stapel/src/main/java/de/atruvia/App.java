6acaea655f913442ea59f686d392e9ce861f2363
Jo Wagner <jowagner@limago.de>
2023-10-11 10:18:58 +0200

    initial commit

---
+++ Tag1_01Stapel/src/main/java/de/atruvia/App.java
@@ -0,0 +1,14 @@
+package de.atruvia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Tag1_01Stapel/src/test/java/de/atruvia/collections/StapelTest.java
@@ -0,0 +1,76 @@
+package de.atruvia.collections;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StapelTest {
+
+
+    private Stapel objectUnderTest;
+    @BeforeEach
+    void setUp() {
+        objectUnderTest = new Stapel();
+    }
+
+    @Test
+    @DisplayName("should return true when isEmpty called on empty Stapel")
+    void isEmpty_emptyStack_returnsTrue() {
+        // Arrange
+
+
+        // Act
+        boolean result = objectUnderTest.isEmpty();
+
+        // Assert
+        assertTrue(result);
+    }
+
+    @Test
+    void isEmpty_notEmptyStack_returnsFalse() throws Exception{
+
+        // Arrange
+
+        objectUnderTest.push(1);
+        // Action
+        var result = objectUnderTest.isEmpty();
+        // Assertion
+
+        assertFalse(result);
+    }
+
+    @Test
+    void push_pushValueIntoStapel_ValueIsOnTopOfStapel() throws StapelException {
+        // Arrange
+
+
+        // Action
+        objectUnderTest.push(1);
+
+        // Assertion
+        assertEquals(1, objectUnderTest.pop());
+    }
+
+    @Test
+    void push_fillupToLimit_noExceptionIsThrown() throws Exception{
+        // Arrange
+        for(int i = 0; i < 9; i++)
+            objectUnderTest.push(1);
+
+        // Action + Assertion
+        assertDoesNotThrow(()->objectUnderTest.push(1));
+    }
+
+    @Test
+    void push_overflow_throwsStapelException() throws Exception{
+        // Arrange
+        for(int i = 0; i < 10; i++)
+            objectUnderTest.push(1);
+
+        StapelException ex = assertThrows(StapelException.class, ()->objectUnderTest.push(1));
+        assertEquals("Overflow", ex.getMessage());
+    }
+
+}
